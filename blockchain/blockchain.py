254ff88660f376879c01b53d0a730f998af0dec2
NimTon <1350339182@qq.com>
2025-10-18 09:27:01 +0800

    feat(blockchain): 增强区块链功能并修复模块导入问题- 修复模块导入路径错误，将 utils.crypto_utils 更正为 utils.utils_crypto - 扩展 add_block 方法支持自定义时间戳参数 - 优化 add_block 方法返回新建区块对象 - 新增 get_transactions_by_address 方法获取指定地址相关交易 - 新增 get_all_transactions 方法获取所有交易并支持用户筛选 - 新增 get_last_block 方法获取最新区块对象并支持空链处理 - 完善区块与交易对象的转换逻辑，确保数据一致性

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -1,5 +1,5 @@
 from blockchain.transaction import Transaction
-from utils.crypto_utils import load_json, save_json
+from utils.utils_crypto import load_json, save_json
 from blockchain.block import Block
 
 # 区块链数据文件路径
@@ -31,7 +31,7 @@ class Blockchain:
         # 将当前区块链保存到文件
         save_json(BLOCKCHAIN_FILE, self.chain)
 
-    def add_block(self, transactions):
+    def add_block(self, transactions, timestamp=None):
         # 验证所有交易
         for tx in transactions:
             # 如果交易对象不是 Transaction 类型，跳过或报错
@@ -43,11 +43,12 @@ class Blockchain:
         # 获取前一个区块的哈希值
         prev_hash = self.chain[-1]['hash']
         # 创建新的区块
-        block = Block(len(self.chain), transactions, prev_hash)
+        block = Block(len(self.chain), transactions, prev_hash, timestamp=timestamp)
         # 将新区块添加到链中
         self.chain.append(block.__dict__)
         # 保存更新后的区块链到文件
         self.save_chain()
+        return block
 
     def get_balance(self, address):
         # 计算指定地址的余额
@@ -67,6 +68,83 @@ class Blockchain:
     def faucet(self, address, amount=50):
         tx = Transaction("SYSTEM", address, amount, "SYSTEM")
         self.add_block([tx])
+
+    def get_transactions_by_address(self, address):
+        """
+        获取与某个地址相关的所有交易（作为发送方或接收方）
+        返回列表，每个元素是交易字典（含所在区块高度和哈希）
+        """
+        related_txs = []
+        for block in self.chain:
+            block_height = block.get("index", 0)
+            block_hash = block.get("hash", "")
+            for tx in block['transactions']:
+                if tx['from'] == address or tx['to'] == address:
+                    tx_info = {
+                        "block_height": block_height,
+                        "block_hash": block_hash,
+                        "tx_hash": tx.get("tx_hash", ""),  # 如果交易中没有tx_hash，可改为计算逻辑
+                        "from": tx['from'],
+                        "to": tx['to'],
+                        "amount": tx['amount']
+                    }
+                    related_txs.append(tx_info)
+        return related_txs
+
+    def get_all_transactions(self, user=None):
+        """
+        获取系统中所有交易。
+        若传入 user 对象，则返回与 user 相关（其所有地址）的交易。
+        """
+        all_txs = []
+        for block in self.chain:
+            block_height = block.get("index", 0)
+            block_hash = block.get("hash", "")
+            for tx in block['transactions']:
+                tx_info = {
+                    "block_height": block_height,
+                    "block_hash": block_hash,
+                    "tx_hash": tx.get("tx_hash", ""),
+                    "from": tx['from'],
+                    "to": tx['to'],
+                    "amount": tx['amount']
+                }
+                all_txs.append(tx_info)
+
+        # 如果 user 存在，筛选与其关联的交易
+        if user and hasattr(user, "addresses"):
+            user_addrs = set(user.addresses)
+            user_txs = [
+                tx for tx in all_txs
+                if tx["from"] in user_addrs or tx["to"] in user_addrs
+            ]
+            return user_txs
+
+        return all_txs
+
+    def get_last_block(self):
+        """
+        返回最新区块对象，如果链为空，则返回 None
+        """
+        self.load_chain()
+        if self.chain:
+            last_block_data = self.chain[-1]
+            transactions = []
+            for tx in last_block_data['transactions']:
+                transactions.append(Transaction(
+                    from_addr=tx['from'],  # 注意这里使用 from_addr
+                    to_addr=tx['to'],
+                    amount=tx['amount'],
+                    signature=tx.get('signature', None)
+                ))
+            # 将字典转回 Block 对象（便于使用 hash 属性）
+            return Block(
+                index=last_block_data['index'],
+                transactions=transactions,
+                previous_hash=last_block_data['previous_hash'],
+                timestamp=last_block_data['timestamp']
+            )
+        return None
 
 
 bc = Blockchain()
