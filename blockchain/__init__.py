8a6d74a76db7fc27bba0fd505bddfaf84ace78d1
NimTon <1350339182@qq.com>
2025-10-26 20:49:09 +0800

    refactor(blockchain):重构交易池管理机制- 移除旧的交易和区块导入方式 - 引入交易池(TransactionPool)统一管理待处理交易 - 创建全局唯一的交易池实例 - 更新模块导出结构以支持新的交易处理流程

--- blockchain/__init__.py
+++ blockchain/__init__.py
@@ -1,7 +1,7 @@
 # blockchain/__init__.py
 from .blockchain import Blockchain
-from .transaction import Transaction
-from .block import Block
+from .transaction_pool import TransactionPool
 
 # 全局唯一区块链实例
 blockchain = Blockchain()
+transaction_pool = TransactionPool()
